c22381c672b347076531c1b4b7ad4918bc799d73
Yaroslav <sixfootus@gmail.com>
2022-08-08 16:29:09 +0300

    Preorder recursive (#2)
    * Added preorder tree traversal method and simple test for these method.
    
    * Update Preorder traveresal method update gitignore.
    
    * Added preorder traversal recursive method. Added bit of comments.
    
    Co-authored-by: Yaroslav Tarakanov <Yaroslav.Tarakanov@litera.com>

--- TestTrees/UnitTest1.cs
+++ TestTrees/UnitTest1.cs
@@ -4,12 +4,36 @@ namespace TestTrees
     [TestClass]
     public class UnitTestTraversal
     {
+
+        /// <summary>
+        /// Generates basic tree [1,null,2,3] 
+        /// </summary>
+        /// <returns>
+        ///         1
+        ///            2
+        ///         3  
+        /// </returns>
+        private TreeNode GenerateBasicTree()
+        {
+            return new TreeNode(1, null, new TreeNode(2, new TreeNode(3, null, null), null));
+        }
         [TestMethod]
         public void TestPreorder()
         {
-            TreeNode root = new TreeNode(1, null, new TreeNode(2, new TreeNode(3, null, null), null));
+            TreeNode root = GenerateBasicTree();
             Solution solution = new Solution();
             var result = solution.PreorderTraversal(root);
+            List<int> expected = new List<int> { 1, 2, 3 };
+            Assert.IsTrue(expected.SequenceEqual(result));
+        }
+
+        [TestMethod]
+        public void TestPreorderRecursive()
+        {
+            TreeNode root = GenerateBasicTree();
+            Solution solution = new Solution();
+            List<int> result = new List<int>();
+            solution.PreorderTraversalRecursive(root, ref result);
             List<int> expected = new List<int> { 1, 2, 3 };
             Assert.IsTrue(expected.SequenceEqual(result));
         }
