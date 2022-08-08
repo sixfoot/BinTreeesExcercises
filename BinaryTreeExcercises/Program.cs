18ab8a022b3ab401a52525cc0beb60d6e71e84a1
Yaroslav <sixfootus@gmail.com>
2022-08-08 15:25:23 +0300

    Preorder (#1)
    * Added preorder tree traversal method and simple test for these method.
    
    * Update Preorder traveresal method update gitignore.
    
    Co-authored-by: Yaroslav Tarakanov <Yaroslav.Tarakanov@litera.com>

---
+++ BinaryTreeExcercises/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+using BinaryTreeExcercises;
+
+public class Solution
+{
+    public IList<int> PreorderTraversal(TreeNode root)
+    {
+		List<int> result = new List<int>();
+		var node = root;
+		List<TreeNode> nodes = new List<TreeNode>();
+		while (node != null || nodes.Count() > 0)
+        {
+			if (node == null) {
+				node = nodes.Last();
+				nodes.Remove(node);
+				node = node.right;
+				continue;
+            }
+			result.Add(node.val);
+			nodes.Add(node);
+			node = node.left;
+        }
+		return result;
+	}
+	static void Main()
+    {
+		Solution s = new Solution();
+		s.PreorderTraversal(new TreeNode());
+    }
+}
+
